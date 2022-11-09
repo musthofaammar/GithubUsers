0f73d226c40dc6720573a265c69d18f85b6210c7
musthofaammar <musthofaammar@gmail.com>
2022-11-09 11:03:49 +0700

    add layout for users

---
+++ app/src/main/java/id/eureka/githubusers/users/presentation/users/UsersActivity.kt
@@ -0,0 +1,12 @@
+package id.eureka.githubusers.users.presentation.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.eureka.githubusers.R
+
+class UsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+    }
+}
