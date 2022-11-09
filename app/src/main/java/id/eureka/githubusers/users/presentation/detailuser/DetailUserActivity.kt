e63d424f6c3b979dd1027786608bc0dbb344aa99
musthofaammar <musthofaammar@gmail.com>
2022-11-09 11:46:41 +0700

    add layout for detail user and repositories

---
+++ app/src/main/java/id/eureka/githubusers/users/presentation/detailuser/DetailUserActivity.kt
@@ -0,0 +1,12 @@
+package id.eureka.githubusers.users.presentation.detailuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.eureka.githubusers.R
+
+class DetailUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_user)
+    }
+}
